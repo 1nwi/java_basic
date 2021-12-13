c27e72defda9248d4ac347852ac044c03613b42c
1nwi <lllhn@naver.com>
2021-12-13 15:34:49 +0900

    [21.12.13] # MethodQuiz1 HomeWork

--- src/day09/MethodQuiz1.java
+++ src/day09/MethodQuiz1.java
@@ -16,6 +16,7 @@ public class MethodQuiz1 {
         foods = temp;
     }
 
+    // foods의 인덱스 번호를 찾는 메서드
     static int indexOf(String search) {
         int index = -1;
         for (int i = 0; i < foods.length; i++) {
@@ -27,6 +28,7 @@ public class MethodQuiz1 {
         return index;
     }
 
+    // 삭제 메서드
     static String remove(String targetFood) {
         // 탐색
         int index = -1;
@@ -51,7 +53,48 @@ public class MethodQuiz1 {
         } else {
             System.out.printf("%s는(은)존재하지 않는 음식명입니다.\n",targetFood);
         }
-        return remove(targetFood);
+        return targetFood;
+    }
+
+    // 추가 메서드
+    static String insert(int insertIdx, String newFood) {
+        String[] temp = new String[foods.length + 1];
+
+        // 2. 기존 데이터를 전부 복사
+        for (int i = 0; i < foods.length; i++) {
+            temp[i] = foods[i];
+        }
+        // 3. 끝데이터부터 뒤로 밀기
+        for (int i = temp.length - 1; i > insertIdx; i--) {
+            temp[i] = temp[i - 1];
+        }
+        // 4. 타겟인덱스에 데이터 추가
+        temp[insertIdx] = newFood;
+        // 5. 주소 변경
+        foods = temp; temp = null;
+
+        return newFood;
+    }
+    // 수정 메서드
+    static String modify(int modifyIdx, String modifyFood) {
+        foods[modifyIdx] = modifyFood;
+        return modifyFood;
+    }
+
+    // 배열의 모든 값 삭제 메서드
+    static void clear() {
+        foods = null;
+    }
+
+    static boolean include(String checkFood) {
+        int index = -1;
+        for (int i = 0; i <foods.length; i++) {
+            if (checkFood.equals(foods[i])) {
+                index = i;
+                break;
+            }
+        }
+        return true;
     }
 
 
@@ -83,13 +126,28 @@ public class MethodQuiz1 {
 
         remove("볶음밥");   // foods 배열에서 볶음밥 제거
         remove("망고");    // 존재하지 않는 음식명입니다 출력
+        System.out.println(Arrays.toString(foods));
+
+        System.out.println("==========================");
 
         insert(3, "파인애플"); // 3번 인덱스에 파인애플 삽입
-        modify(2, "닭갈비"); // 2번 인덱스 데이터 닭갈비로 수정
-        clear(); // foods 배열 모든 데이터 삭제
+        System.out.println(Arrays.toString(foods));
 
+        System.out.println("==========================");
+
+        modify(2, "닭갈비"); // 2번 인덱스 데이터 닭갈비로 수정
+        System.out.println(Arrays.toString(foods));
+
+        System.out.println("==========================");
 
         boolean flag = include("파스타"); // 파스타 발견시 true 리턴
+        System.out.println(flag);
+
+        System.out.println("==========================");
+
+        clear(); // foods 배열 모든 데이터 삭제
+        System.out.println(Arrays.toString(foods));
+
 
     }
 }
