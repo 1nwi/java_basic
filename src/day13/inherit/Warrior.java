b1e782634f6af3e25ad54490183d8d0a1ac6f48f
1nwi <lllhn@naver.com>
2021-12-28 15:32:51 +0900

    [21.12.28] # 다형성 homework

--- src/day13/inherit/Warrior.java
+++ src/day13/inherit/Warrior.java
@@ -20,8 +20,25 @@ public class Warrior extends Player {
         System.out.println(("# 분노: " + rage));
     }
 
-    public void fireRush() {
+    public void fireRush(Player target) {
+        System.out.println("=========================");
+        System.out.printf("%s님이 %s님에게 FireRush를 시전했습니다!\n",this.name,target.name);
+        int damage;
 
+        if (target instanceof Mage) {
+            damage = 20;
+            System.out.printf("%s(마법사)님이 %d의 피해를 입었습니다.\n",target.name,damage);
+            target.hp -= damage;
+        } else if (target instanceof Hunter) {
+            damage = 15;
+            System.out.printf("%s(사냥꾼)님이 %d의 피해를 입었습니다.\n",target.name,damage);
+            target.hp -= damage;
+        } else if (target instanceof Warrior) {
+            damage = 10;
+            System.out.printf("%s(전사)님이 %d의 피해를 입었습니다.\n",target.name,damage);
+            target.hp -= damage;
+        }
+        System.out.printf("%s님의 현재 체력: %d\n",target.name,target.hp);
     }
 
 }
