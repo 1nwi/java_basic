de5dee9a7b2c8b1a399b28bdf1f092b4041415d5
1nwi <lllhn@naver.com>
2022-01-04 12:51:10 +0900

    [22.1.4] # 컬렉션 학습

---
+++ src/com/kh/practice/list/library/view/BookMenu.java
@@ -0,0 +1,36 @@
+package com.kh.practice.list.library.view;
+
+import com.kh.practice.list.library.controller.BookController;
+
+import java.util.Scanner;
+
+public class BookMenu {
+
+    BookController bc = new BookController();
+    Scanner sc = new Scanner(System.in);
+
+    public void mainMenu(){
+        System.out.println("== Welcome KH Library==");
+    }
+
+    public void insertBook() {
+
+    }
+
+    public void selectList() {
+
+    }
+
+    public void searchBook() {
+
+    }
+
+    public void deleteBook() {
+
+    }
+
+    public void ascBook() {
+
+    }
+
+}
